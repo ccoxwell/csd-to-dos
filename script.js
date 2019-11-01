233fab8efe1d9aa77fcd21b9bae6d8aa3a0d0617
Carrie Coxwell <carrie.coxwell@gmail.com>
2019-11-01 07:22:59 -0500

    fixed audio volume

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let powerDown = new Audio("/assets/smb_pipe.wav");
 let stageClear = new Audio("/assets/smb_stage_clear.wav");
 let previousAudio = mushroom;
 
+[mushroom, powerDown, stageClear].map(audio => audio.volume = 0.1);
+
 let todoElementsList = Array.from(document.querySelectorAll("#to-do li"));
 console.log(todoElementsList);
 let todoObjList = todoElementsList.map(todo => {
